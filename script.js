9e03ae777fccc600c25106e783de850bf3b0c1be
Nini76gaby <gabydiana76@yahoo.de>
2024-05-01 17:28:48 +0200

    main content

---
+++ script.js
@@ -0,0 +1,44 @@
+let allDishes =[
+{
+    name:"Pizza Margherita",
+    description:"mit Mozarella,Tomatensoße und Basilikum",
+    plusImage:"icon/plus.png",
+    price:8.99,
+    dishesImage:"img/margherita.jpg"
+},
+{
+    name:"Pizza Prosciutto",
+    description:"mit Schinken,Mozarella und Pepperoni",
+    plusImage:"icon/plus.png",
+    price:9.99,
+    dishesImage:"img/mozarella.jpg"
+},
+{
+    name:"Pizza Salami",
+    description:"mit Schinken,Mozarella und Pepperoni",
+    plusImage:"icon/plus.png",
+    price:11.99,
+    dishesImage:"img/salami.jpg"
+},
+{
+    name:"Pizza Spinaci",
+    description:"mit Spinat und Käse",
+    plusImage:"icon/plus.png",
+    price:10.99,
+    dishesImage:"img/spinaci.jpg"
+},
+{
+    name:"Pizza Mexicana",
+    description:"mit Mais,Paprika und Mozarella",
+    plusImage:"icon/plus.png",
+    price:11.99,
+    dishesImage:"img/margherita.jpg"
+},
+{
+    name:"Pizza Vegetaria",
+    description:"mit allerlei Gemüse",
+    plusImage:"icon/plus.png",
+    price:12.99,
+    dishesImage:"img/vegetaria.jpg"
+}
+]
