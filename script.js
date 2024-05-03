3bfab3bd62d724e66642d9917caa24ce180694a1
Nini76gaby <gabydiana76@yahoo.de>
2024-05-03 15:10:13 +0200

    changing JS

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 let menus =[
-{
-    name:"Pizza Margherita",
-    description:"mit Mozarella,Tomatensoße und Basilikum",
-    plusImage:"icon/plus.png",
-    minusImage:"icon/minus.png",
-    price:8.99,
-    amount:1,
-    dishesImage: 'img/pizzamargherita.jpg'
-},
-{
-    name:"Pizza Prosciutto",
-    description:"mit Schinken,Mozarella und Pepperoni",
-    plusImage:"icon/plus.png",
-    minusImage:"icon/minus.png",
-    price:9.99,
-    amount:1,
-    dishesImage:"img/mozarella.jpg"
-},
-{
-    name:"Pizza Salami",
-    description:"mit Schinken,Mozarella und Pepperoni",
-    plusImage:"icon/plus.png",
-    minusImage:"icon/minus.png",
-    price:11.99,
-    amount:1,
-    dishesImage:"img/salami.jpg"
-},
-{
-    name:"Pizza Spinaci",
-    description:"mit Spinat und Käse",
-    plusImage:"icon/plus.png",
-    minusImage:"icon/minus.png",
-    price:10.99,
-    amount:1,
-    dishesImage:"img/spinaci.jpg"
-},
-{
-    name:"Pizza Mexicana",
-    description:"mit Mais,Paprika und Mozarella",
-    plusImage:"icon/plus.png",
-    minusImage:"icon/minus.png",
-    price:11.99,
-    amount:1,
-    dishesImage:"img/pizzamargherita.jpg"
-},
-{
-    name:"Pizza Vegetaria",
-    description:"mit allerlei Gemüse",
-    plusImage:"icon/plus.png",
-    minusImage:"icon/minus.png",
-    price:12.99,
-    amount:1,
-    dishesImage:"img/vegetaria.jpg"
-}
-];
-
+    {
+        name:"Pizza Margherita",
+        description:"mit Mozarella,Tomatensoße und Basilikum",
+        plusImage:"./icon/plus.png",
+        minusImage:"./icon/minus.png",
+        price:8.99,
+        amount:1,
+        dishesImage: './img/pizzamargherita.jpg'
+    },
+    {
+        name:"Pizza Prosciutto",
+        description:"mit Schinken,Mozarella und Pepperoni",
+        plusImage:"./icon/plus.png",
+        minusImage:"./icon/minus.png",
+        price:9.99,
+        amount:1,
+        dishesImage:"./img/mozarella.jpg"
+    },
+    {
+        name:"Pizza Salami",
+        description:"mit Schinken,Mozarella und Pepperoni",
+        plusImage:"./icon/plus.png",
+        minusImage:"./icon/minus.png",
+        price:11.99,
+        amount:1,
+        dishesImage:"./img/salami.jpg"
+    },
+    {
+        name:"Pizza Spinaci",
+        description:"mit Spinat und Käse",
+        plusImage:"./icon/plus.png",
+        minusImage:"./icon/minus.png",
+        price:10.99,
+        amount:1,
+        dishesImage:"./img/spinaci.jpg"
+    },
+    {
+        name:"Pizza Mexicana",
+        description:"mit Mais,Paprika und Mozarella",
+        plusImage:"./icon/plus.png",
+        minusImage:"./icon/minus.png",
+        price:11.99,
+        amount:1,
+        dishesImage:"./img/pizzamargherita.jpg"
+    },
+    {
+        name:"Pizza Vegetaria",
+        description:"mit allerlei Gemüse",
+        plusImage:"./icon/plus.png",
+        minusImage:"./icon/minus.png",
+        price:12.99,
+        amount:1,
+        dishesImage:"./img/vegetaria.jpg"
+    }
+    ];
+let shoppingBasket = []
 
 function init(){
   render();
@@ -62,27 +62,45 @@ function init(){
 
 function render(){
     let content = document.getElementById('content');
-    content.innerHTML='';
+    content.innerHTML=''; //Inhalt leeren
 
-    for (let i = 0; i < menus.length; i++) {
+    for (let i = 0; i < menus.length; i++) { // durch Array iterieren
         const menu = menus[i];
         content.innerHTML = `
-        <div class="menu-item">
-             <img src="${menu.dishesImage}" alt="${menu.name}" />
-             <h3>${menu.name}</h3>
-             <p>${menu.description}</p>
-             <p>Price: ${menu.price} €</p>
-             <img src="${menu.plusImage}" alt="Add to cart" />
-             <img src="${menu.minusImage}"/>
-             <button onclick="addToCart(${i})">Add to cart</button>
+        <div class="dishes-div">
+        <div class="dishes-name-div">
+            <h4 class="menu-name">${menu.name}</h4>
+            <img class="plusImg" src="/icon/plus.png" onclick="addFoodToBasket(${i})" alt="Plus">
         </div>
-        `   
+        <div class="dishes-description-img-div">
+            <div>
+                <p class="dishes-description">${menu.description}</p>
+                <p class="dishes-price"><b>${menu.price.toFixed(2)} €</b></p>
+            </div>
+            <div>
+                <img class="dishes-img" src="${menu.dishesImage}" alt="${menu.name}">
+            </div>
+        </div>
+    </div>`;
+}
+}
+function getMenuIndex(index) { 
+    
+    return shoppingBasket.indexOf(index); //Überprüft ob in Warenkorb schon was drin ist
+
+function renderBasket() {
+    let basketContent = document.getElementById('basket-content');
+    basketContent.innerHTML = "";
+    
+    for (let i = 0; i < shoppingBasket.length; i++) {
+        basketContent.innerHTML += `
+            <div class="basket-item">
+                <span>${shoppingBasket[i]}</span>
+                <span>${amountBasket[i]}</span>
+            </div>
+        `;
     }
 }
-function getMenuIndex(index) { //Überprüft ob in Warenkorb schon was drin ist
-    return shoppingBasket.indexOf(index);
-  }
-  
 function addFoodToBasket(i){ // Food hinzufügen
     let menu =menus[i];
     let menuName = menu.name;
@@ -96,5 +114,6 @@ function addFoodToBasket(i){ // Food hinzufügen
     } else{
         amountBasket[index]++; // Menge des vorhandenen Gerichts im Warenkorb erhöhen
     }
-    render();
+    renderBasket();
+}
 }
